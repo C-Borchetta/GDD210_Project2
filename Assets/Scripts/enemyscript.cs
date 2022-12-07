b9616012abcaf3f3209dfd5278dc9e48007b89da
C-Borchetta <cabman035@gmail.com>
2022-12-07 13:43:09 -0500

    enemy script

--- Assets/Scripts/enemyscript.cs
+++ Assets/Scripts/enemyscript.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class enemyscript : MonoBehaviour
 {
+    public Transform target;
+    public float moveSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        target = FindObjectOfType<PlayerMovement>().transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position -= (transform.position - target.transform.position).normalized * moveSpeed * Time.deltaTime;
     }
 }
